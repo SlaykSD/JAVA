43232913ebbdbe2e6ac8bbe1029a90a5da98b193
SlaykSD <60920077+SlaykSD@users.noreply.github.com>
2021-09-15 16:54:00 +0300

    Initial commit

---
+++ src/main/java/ru/mephi/map/MyMap.java
@@ -0,0 +1,9 @@
+package ru.mephi.map;
+
+public class MyMap {
+    public static void main(String... args)
+    {
+
+        System.out.println("Hello world, this is lab 2, I wonna write the map here");
+    }
+}
